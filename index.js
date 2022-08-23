67c4086afa4a0b9ada515928fa1207f98193a67f
mercylynn <mercy.kamene@student.moringaschool.com>
2022-08-23 04:43:16 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded",() => {
+    console.log("The DOM has loaded")
+    let p=document.querySelector("#text")
+    p.textContent="This is really cool!"
+  })
+  console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
